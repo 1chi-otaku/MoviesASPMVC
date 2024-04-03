2c2ef5b0245968de1bfc857b0dd8284ca80f76e5
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-04-03 19:04:27 +0100

    Add project files.

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesContext _db;
+
+        public MoviesController(MoviesContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Movies> movies = await Task.Run(() => _db.Movies);
+            ViewBag.Movies = movies;
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int Year { get; set; }
+        public string? Director { get; set; }
+        public string? Genre { get; set; }
+        public string? Img { get; set; }
+        public string? Info { get; set; }
+
+    }
+}
