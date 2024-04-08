fb5f84e878b47a55a4d3caaf8a6103ce227e7dc0
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-04-08 14:13:11 +0100

    Beginning

--- WebApplication1/Models/Movies.cs
+++ WebApplication1/Models/Movies.cs
@@ -1,14 +1,31 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Movies
     {
+        
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title can not be empty!")]
         public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Year can not be empty!")]
+        [Range(868, 2025, ErrorMessage = "Invalid Year")]
         public int Year { get; set; }
+
+        [Required(ErrorMessage = "Director can not be empty!")]
         public string? Director { get; set; }
+
+        [Required(ErrorMessage = "Genre can not be empty!")]
         public string? Genre { get; set; }
+
+
         public string? Img { get; set; }
+
+        [StringLength(512, MinimumLength = 1, ErrorMessage = "Info length must be between 1 and 512 characters.")]
         public string? Info { get; set; }
+
 
     }
 }
